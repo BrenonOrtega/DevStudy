1e71cf1b1c0dcc14d4c036a21150c4f9d43baad7
Ortega <br6u7009@festo.net>
2020-11-23 11:03:28 -0300

    Exercícios com tratamento de dados parse/Number e toLocaleString.

--- CursoJS/Aula6/ex002/JS/javascript.js
+++ CursoJS/Aula6/ex002/JS/javascript.js
@@ -11,6 +11,38 @@ var nome = window.prompt("Qual é seu nome?")
 alert("olá " + nome + " isso foi escrito com concatenação.")
 
 //exemplo utilizando o template string.
-var teste = alert(`alo srº ${nome} como está? isso foi escrito utilizando template string`)
+alert(`alo srº ${nome} como está? isso foi escrito utilizando template string`)
+
+//Será pedido dois números e feito a soma dos dois.
+//comando parseInt() transforma o que naturalmente vem como string do prompt em númeral, é feita a soma e então apresentado no alert
+var n1 = parseInt(window.prompt("Insira um número por favor."))
+
+var n2 = parseInt(window.prompt("Insira um número por favor."))
+
+var s = n1+n2
+
+alert(`A soma dos número é ${s}`)
+
+//Agora mostrarei um valor monetário em dolar, euro e real, utilizando o conversor Number.
+var valor = Number(window.prompt("Conversor de Moeda.", "Insira um valor, por favor"))
+
+//converte o valor para dolar adicionando o cifrão.
+var cotDolar = valor.toLocaleString('en', {style: 'currency', currency: "USD"})
+
+var cotEuro = valor.toLocaleString("EN", {style: "currency", currency: "EUR"})
+
+//lb para quebra de linha
+var lb = '\n'
+
+//coloca na variavel msg as frases e os valores inseridos pelo usuário convertidos.
+msg = `O valor no formato dolar é ${cotDolar} ${lb}O valor no formato euro é: ${cotEuro}.`
+
+alert(msg)
+
+this.Document.getElementById("myp2").innerHTML = msg
+
+function myfunction(){
+    document.getElementById("demo").innerHTML= "Voi-lá, um clique magistral com toda a certeza."
+}
 
 
