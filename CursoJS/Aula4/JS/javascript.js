7636f70191f9bd7642bb252b8aeed31f3405291a
BrenonOrtega <brenon.ortega@hotmail.com>
2020-11-22 21:56:36 -0300

    Feitos alguns exercícios com o curso em vídeo, há coisas não funcionais na aula 6.

---
+++ CursoJS/Aula4/JS/javascript.js
@@ -0,0 +1,11 @@
+var confirma
+var nome
+window.alert("dahora, olá mundo!")
+
+window.confirm("Está gostando?", confirma)
+
+window.prompt("Qual é seu nome?", nome)
+
+if (confirma ==true){
+    window.alert(nome)
+}
