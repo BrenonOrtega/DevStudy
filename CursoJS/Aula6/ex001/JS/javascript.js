aa8681e074ce2de0cba6376debd0023ba055975b
Ortega <br6u7009@festo.net>
2020-11-23 07:57:29 -0300

    Estudando Javascript, até o momento innerhtml não funcional.

---
+++ CursoJS/Aula6/ex001/JS/javascript.js
@@ -0,0 +1,21 @@
+//Inserção de dados nas variáveis.
+window.alert("dahora, olá mundo!")
+
+window.confirm("Está gostando de fazer programação?")
+
+//guarda o resultado do prompt na variavel nome.
+//poderia criar a variável usando var, let ou const mas altera o escopo da variável.
+var nome = window.prompt("Qual é seu nome?")
+
+//Exemplo fzd o alert concatenando
+alert("olá " + nome)
+
+//exemplo utilizando o template string.
+var teste = alert(`alo srº ${nome} como está?`)
+
+//até o momento não funcional.
+function myfunction(){
+var x = document.getElementById("p1").innerHTML = "Paragraph change"
+}
+
+document.getElementById("p1").innerHTML = "Hello Dolly.";
---
+++ CursoJS/Aula6/ex002/JS/javascript.js
@@ -0,0 +1,16 @@
+//Inserção de dados nas variáveis.
+window.alert("dahora, olá mundo!")
+
+window.confirm("Está gostando de fazer programação?")
+
+//guarda o resultado do prompt na variavel nome.
+//poderia criar a variável usando var, let ou const mas altera o escopo da variável.
+var nome = window.prompt("Qual é seu nome?")
+
+//Exemplo fzd o alert concatenando
+alert("olá " + nome + " isso foi escrito com concatenação.")
+
+//exemplo utilizando o template string.
+var teste = alert(`alo srº ${nome} como está? isso foi escrito utilizando template string`)
+
+
