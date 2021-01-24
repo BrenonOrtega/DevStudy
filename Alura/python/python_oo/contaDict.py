705c5ed91dd92b88796763dbaf7d74f49022f4d0
BrenonOrtega <brenon.ortega@hotmail.com>
2021-01-24 09:32:32 -0300

    removido o uso de dict no _repo e criados atributos privados (ex: __limite, __saldo) .

--- Alura/python/python_oo/contaDict.py
+++ Alura/python/python_oo/contaDict.py
@@ -1,9 +1,10 @@
 class Conta:
     def __init__ (self, titular, saldo=0, agencia="0000-0", limite = 1000):
+        self._conta = {"titular": titular, "saldo": saldo, "agencia":agencia, "limite": limite}
         self.__titular = titular
         self.__saldo = saldo
         self.__agencia = agencia
-        self.__limite = limite 
+        self.limite = limite 
         
 
     def saque(self, quantia):
@@ -22,44 +23,43 @@ class Conta:
             except:
                 raise "A conta destino deve ser um objeto do tipo Conta."
         else:
-            print("Você não possui saldo para transferir esta quantia!")
+            print("Você não possui saldo para transferir esta quantia!)
     
     def extrato (self):
         print (f"||Titular: {self.titular}||")
         print (f"||Saldo: R${self.saldo}  ||")
         print (f"||Agencia: {self.agencia}||")
         print (f"||Limite: R${self.limite}||")
-        self.
 
     @property
     def titular(self):
-        return self.__titular
+        return self._conta["titular"]
     
     @property  
     def saldo(self):
-        return self.__saldo
+        return self._conta["saldo"]
 
     @saldo.setter
     def saldo(self, novo_saldo):
-        self.__saldo = novo_saldo
+        self._conta["saldo"] = novo_saldo
         self.saldo
 
     @property
     def agencia(self):
-        return self.__agencia
+        return self._conta["agencia"]
     
     @agencia.setter
     def agencia(self, nova_agencia):
-        self.__agencia = nova_agencia
+        self._conta["agencia"] = nova_agencia
         self.agencia
     
     @property
     def limite (self):
-        return self.__limite
+        return self._conta["limite"]
     
     @limite.setter
     def limite (self, novo_limite):
-        self.__limite
+        self._conta["limite"] = novo_limite
         print(self.limite) 
 
     
