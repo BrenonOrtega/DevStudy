5e2c941a25f413c5de2a36efdbded5c5b06f78a3
BrenonOrtega <brenon.ortega@hotmail.com>
2021-02-07 15:26:44 -0300

    encerramento do curso python3 - avançando na orientação a objetos

--- Alura/python/python_oo2/Model.py
+++ Alura/python/python_oo2/Model.py
@@ -25,7 +25,8 @@ class Filme(ProducaoCinematografica):
         super().__init__(nome, ano)
         self.duracao = duracao
 
-     
+    def __str__(self):
+        return f"nome: {self.nome}, ano: {self.ano}, duração: {self.duracao}, likes: {self.likes}."
 
 
 class Serie(ProducaoCinematografica):
@@ -33,16 +34,44 @@ class Serie(ProducaoCinematografica):
         super().__init__(nome, ano)
         self.temporadas = temporadas
 
+    def __str__(self):
+        return f'nome: {self.nome}, ano:{self.ano}, temporadas: {self.temporadas}, likes: {self.likes}'
+
+
+class Playlist:
+    def __init__(self, nome, programacao):
+        self.nome = nome
+        self._programacao = programacao
+
+    def __getitem__(self, item):
+        return self._programacao[item]
+
+    def __len__(self):
+        return len (self._programacao)
+
 
 vingadores = Filme("vingadores-guerra infinita", 2018, 160)
+troia = Filme("troia", 2010, 120)
+
 demolidor = Serie("demolidor da netflix", 2015, 3)
-
-vingadores.dar_like()
-demolidor.dar_like() 
-
+supernatural = Serie("supernatural", 2007, 9)
 
 vingadores.nome = 'vingadores-guerra infinita parte 2'
-print (vingadores.nome)
 
-print(f'{vingadores.nome} - {vingadores.duracao}: {vingadores.likes}')
-print(f'{demolidor.nome} - {demolidor.temporadas}: {demolidor.likes}')
+vingadores.dar_like()
+vingadores.dar_like()
+troia.dar_like()
+troia.dar_like()
+supernatural.dar_like()
+supernatural.dar_like()
+demolidor.dar_like() 
+
+filmes_series = [vingadores, demolidor, troia, supernatural]
+
+minha_playlist = Playlist('fim de semana', filmes_series)
+
+for programa in minha_playlist:
+    print(programa)
+ 
+'''corrigir len '''
+print(f'len dessa lista é {len(minha_playlist)}')
